e6adb45b5d1244aa21dea24a0da42329ce2a3f7a
be-knucklehead <b220062@iiit-bh.ac.in>
2023-02-28 21:49:50 +0530

    filtering added in code

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+print("Put some skill that you are familiar with")
+familiar_skill = input(">")
+print(f"Filtering out {familiar_skill}")
+
+
 url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation="
 http_content = requests.get(url)
 http_text = http_content.text
@@ -9,5 +14,12 @@ soup = BeautifulSoup(http_text, "lxml")
 jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
 
 for job in jobs:
-    post_date = soup.find("span", class_="sim-posted")
-    print(post_date)
+    post_date = job.find('span', class_="sim-posted").text
+    if "few" in post_date:
+        company_name = job.find('h3', class_="joblist-comp-name").text.replace(" ", "")
+        skills = job.find('span', class_="srp-skills").text.replace(" ", "")
+        job_link = job.header.h2.a['href']
+        if familiar_skill in skills:
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Required Skills: {skills.strip()}")
+            print(f"More info: {job_link}")
